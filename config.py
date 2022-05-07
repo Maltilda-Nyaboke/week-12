406540cbfe63cf891afd5ff08c3c4f1af849bcce
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-07 11:39:56 +0300

    style:create boilerplates

---
+++ config.py
@@ -0,0 +1,14 @@
+class Config:
+    pass
+
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+ Debug = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
