a0328baaa4c819a25b1fcd37eaf35539a87a6e70
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-08 03:31:12 +0300

    style:instanciate manager

--- manage.py
+++ manage.py
@@ -1,8 +1,14 @@
+from distutils.cmd import Command
+from multiprocessing import managers
 from app import create_app
 from flask import Flask
+from flask_script import Manager,Server
 
 
 
-app = create_app('development'):
+app = create_app('development')
 
 app= Flask(__name__)
+
+manager= Manager(app)
+manager.add.command(Server, Manager)
