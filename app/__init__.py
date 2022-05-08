dbec1a85157606b576b16e74cad28081c52efffe
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-08 05:55:45 +0300

    style:initialize email configurations

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ from flask_login import LoginManager
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 
 db= SQLAlchemy()
-
+mail = Mail()
 photos = UploadSet('photos',IMAGES)
 bootstrap = Bootstrap()
 
@@ -17,6 +18,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
     configure_uploads(app,photos)
 
     from .auth import auth as auth_blueprint
--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+export MAIL_USERNAME='maltildabosibori@gmail.com'
+export MAIL_PASSWORD='kingedqueen2020#'
 python3.9 manage.py server
