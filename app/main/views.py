309abd4aa42d0cddc4385bb0103bed14780269a8
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-08 16:11:44 +0300

    style:add view profile function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from . import main
-from flask import render_template
+from flask import render_template,request,url_for,abort
 from flask_login import login_required
+from ..models import  User
 
 
 
@@ -8,5 +9,14 @@ from flask_login import login_required
 def index():
 
     return render_template('index.html')
+
+@main.route('/user/<uname>') 
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+    return render_template('profile/profile.html', user = user)
+
 
     
--- app/models.py
+++ app/models.py
@@ -19,7 +19,8 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255),nullable=False,unique = True)
     email = db.Column(db.String(255),nullable=False,unique=True)
-    bio = db.Column(db.String(255),nullable= False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255),nullable=False)
 
     @property
