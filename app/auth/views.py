934b2f1382074558f48b1a941e9ac01be77d18a7
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-08 12:05:29 +0300

    style:create register file

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from turtle import title
+from flask import render_template,redirect, url_for
 from . import auth
+from .forms import RegistrationForm
+from ..import db
+from ..models import  User
 
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)  
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth/login')) 
+        title = "New account"
+    return render_template('auth/register.html',registration_form =form) 
