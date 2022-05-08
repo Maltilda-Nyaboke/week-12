d3d9679cca072c817e2057841c0aa3849617fc8a
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-08 08:16:16 +0300

    feature:initialize the login function

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,10 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 db= SQLAlchemy()
 mail = Mail()
 photos = UploadSet('photos',IMAGES)
