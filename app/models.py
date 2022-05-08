0cff17a7aa936798cf44e476336f86e75f166aa6
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-08 06:36:08 +0300

    feature:authenticate password

--- app/models.py
+++ app/models.py
@@ -15,6 +15,17 @@ class User(db.Model):
     email = db.Column(db.String(255),nullable=False,unique=True)
     pass_secure = db.Column(db.String(255),nullable=False)
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password) 
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)      
+
 
     def save_user(self):
         db.session.add(self)
