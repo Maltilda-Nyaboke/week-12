8c01c83fd13c5208d99c22d623ec5ac13f7252fd
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-05-08 16:04:58 +0300

    feature:enable logout function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,23 @@
 from turtle import title
 from flask import render_template,redirect, url_for,flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from .forms import RegistrationForm,LoginForm
 from ..import db
 from ..models import  User
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)  
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth/login')) 
+    title = "New account"
+    return render_template('auth/register.html',registration_form =form,title=title) 
+
 
 
 @auth.route('/login',methods = ['GET', 'POST'])
@@ -21,13 +34,10 @@ def login():
     
     return render_template('auth/login.html',login_form=login_form,title=title)
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)  
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth/login')) 
-        title = "New account"
-    return render_template('auth/register.html',registration_form =form) 
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
